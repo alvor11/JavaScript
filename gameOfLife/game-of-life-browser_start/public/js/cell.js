a7bb93d620171fb41986910d0c862853faaa1b0c
alvor11 <v.tyupa@mail.ru>
2017-12-10 09:42:21 +0200

    next commit

--- gameOfLife/game-of-life-browser_start/public/js/cell.js
+++ gameOfLife/game-of-life-browser_start/public/js/cell.js
@@ -4,30 +4,35 @@ class Cell {
         this.element = element;
         this.row = row;
         this.col = col;
-        this.alive = alive;
+        this._alive = alive;
         // доинициализировать свойства в методе `init`
         this.init();
     }
 
     get alive() {
         // вернуть состояние клетки
-        return this.alive;
+        return this._alive;
     }
 
     set alive(value) {
         // обвновить состояние клетки
-        value? this.alive = true: this.alive = false;
+        this._alive = value;
         // если клетка жива, присвоить класс `alive`
         // в противном случае убрать класс `alive`
+        this._alive? this.element.classList.add('alive'): this.element.classList.remove('alive');
     }
 
     init() {
         // элементу присвоить класс `cell`
+        this.element.className = 'cell';
         // если клетка жива присвоить ей класс `alive`
+        this.alive = this._alive;
         // подписаться на событие `click` у элемента
+        this.element.addEventListener('click', this.handleClick.bind(this));
     }
 
     handleClick(event) {
         // при нажатии на клетку изменить ее состояние
+        this.alive = !this.alive;
     }
 }
